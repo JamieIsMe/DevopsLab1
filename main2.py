7f2e893c8929feed5ef719378401032bd3405536
Sean O'Connor <151649707+Seanmpoc@users.noreply.github.com>
2024-09-27 11:56:26 +0100

    Added max and min

--- main2.py
+++ main2.py
@@ -1,7 +1,13 @@
-n=0
-s=0
-for num in input().split():
-    n+=1
-    s+=int(num)
-s/=n
-print(s)
+x = input("Enter number 1: ")
+y = input("Enter number 2: ")
+z = input("Enter number 3: ")
+
+sum = x+y+z
+
+avg = sum/3
+
+maxNum = max(x,y,z)
+minNum = min(x,y,z)
+
+print(avg)
+print(maxNum,minNum)
